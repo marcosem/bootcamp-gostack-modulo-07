65e09bcc7381cd8e4984799153e327a53934ed93
Marcos Mathias <marcos-github@memathias.com>
2019-07-15 20:18:14 -0300

    Converted Home Class to Function with hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux'; // connect this component to redux state
 import { bindActionCreators } from 'redux';
@@ -11,86 +11,72 @@ import { ProductList, AddToCartButton } from './styles';
 
 // move export defult to ouside fo class to be able to use connect from redux
 // export default class Home extends Component {
-class Home extends Component {
-  static propTypes = {
-    addToCartRequest: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-    }).isRequired,
-    amount: PropTypes.number.isRequired,
-  };
+function Home({ amount, addToCartRequest }) {
+  const [products, setProducts] = useState([]); // Replace the state, need to be done for each element in the state
 
-  state = {
-    products: [],
-  };
+  // Replace the componentDidMount
+  useEffect(() => {
+    // Declare an async function
+    async function loadProducts() {
+      const response = await api.get('products');
 
-  async componentDidMount() {
-    const response = await api.get('products');
+      // Avoit to call function inside the render, it is better to do like this way inside the componentDidMount
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
 
-    // Avoit to call function inside the render, it is better to do like this way inside the componentDidMount
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
+      setProducts(data);
+    }
 
-    this.setState({ products: data });
-  }
+    // Run the declared function
+    loadProducts();
+  }, []);
+
+  // useEffect(() => {}, []) -> run only once, just like DidMount did;
+  // useEffect(() => {}, [products]) -> run only when there is a change in the products;
 
   // Function handleAddProduct receives the product as argument
   // Every component connected to redux receives a property called "this.props.dispatch"
   // it basically works to do actions to redux
 
-  handleAddProduct = id => {
-    // changed this after mapDispatchToProps being added to the end of this module
-    // const { dispatch } = this.props;
-    const { addToCartRequest } = this.props;
-
-    // changed this after mapDispatchToProps being added to the end of this module
-    // dispatch(CartActions.addToCard(product));
-    // Changed to support Saga
-    // addToCart(product);
+  // Only use "useCallback" hooks when the function handle some state or dynamic properties
+  // handleAddProduct = id => {
+  function handleAddProduct(id) {
+    // const { addToCartRequest } = this.props; -> Removed after added the hooks
     addToCartRequest(id);
-
-    // how to navigate using react router dom:
-    // this.props.history.push('/cart');
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
-
-    return (
-      <ProductList>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormatted}</span>
-
-            <AddToCartButton onClick={() => this.handleAddProduct(product.id)}>
-              <div>
-                <MdAddShoppingCart size={16} color="#fff" />
-                {amount[product.id] || 0}
-              </div>
-              <span>ADD TO CART</span>
-            </AddToCartButton>
-          </li>
-        ))}
-      </ProductList>
-    );
   }
+
+  // render() {
+  // const { products } = this.state; -> variable created in the function init
+  // const { amount } = this.props; -> moved to function parameters
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+
+          <AddToCartButton onClick={() => handleAddProduct(product.id)}>
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" />
+              {amount[product.id] || 0}
+            </div>
+            <span>ADD TO CART</span>
+          </AddToCartButton>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
 
-// Proptypes
-/*
-"id": 1,
-"title": "Tênis de Caminhada Leve Confortável",
-"price": 179.9,
-"image": "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
-"amount": 1,
-"priceFormatted": "R$ 179,90",
-"subtotal": "R$ 179,90"
-*/
-// function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
+Home.propTypes = {
+  addToCartRequest: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+  amount: PropTypes.number.isRequired,
+};
 
 // Convert Redux actions to properties
 const mapDispatchToProps = dispatch =>
